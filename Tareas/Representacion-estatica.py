209c53737647751d4cc11a24610a2817a79770a8
Milton Choquehuanca <85814575+Milton-CHR@users.noreply.github.com>
2025-08-28 14:05:35 -0400

    Update Representacion-estatica.py

--- Tareas/Representacion-estatica.py
+++ Tareas/Representacion-estatica.py
@@ -1,23 +1,23 @@
 # Representacion estatica
-libros = {} 
+libros={} 
 def agregar_libro(titulo, autor, isbn, genero):
-    libros[isbn] = {
-        "titulo": titulo,
-        "autor": autor,
-        "genero": genero
+    libros[isbn]={
+        "titulo":titulo,
+        "autor":autor,
+        "genero":genero
     }
 
 def buscar_libro(dato):
-    resultados = []
+    resultados=[]
     for codigo, info in libros.items():
-        if dato in info["titulo"] or dato in info["autor"] or dato == codigo:
+        if dato in info["titulo"] or dato in info["autor"] or dato==codigo:
             resultados.append(info)
-    if resultados == []:
+    if resultados==[]:
         print("no encontre nada")
     return resultados
 
 def mostrar_libros():
-    if len(libros) == 0:
+    if len(libros)==0:
         print("no hay libros guardados")
     else:
         for codigo, info in libros.items():
@@ -30,5 +30,5 @@ agregar_libro("El principito", "Antoine", "72534", "infantil")
 print("TODOS LOS LIBROS:")
 mostrar_libros()
 
-print("\nBUSQUEDA:")
+print("BUSQUEDA:")
 print(buscar_libro("Marquez"))
