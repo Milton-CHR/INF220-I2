4c7e5f8ef13e668f5cb6df50b7a31521f28a4603
Milton Choquehuanca <85814575+Milton-CHR@users.noreply.github.com>
2025-08-28 13:59:02 -0400

    Create Representacion-estatica.py

---
+++ Tareas/Representacion-estatica.py
@@ -0,0 +1,34 @@
+# Representacion estatica
+libros = {} 
+def agregar_libro(titulo, autor, isbn, genero):
+    libros[isbn] = {
+        "titulo": titulo,
+        "autor": autor,
+        "genero": genero
+    }
+
+def buscar_libro(dato):
+    resultados = []
+    for codigo, info in libros.items():
+        if dato in info["titulo"] or dato in info["autor"] or dato == codigo:
+            resultados.append(info)
+    if resultados == []:
+        print("no encontre nada")
+    return resultados
+
+def mostrar_libros():
+    if len(libros) == 0:
+        print("no hay libros guardados")
+    else:
+        for codigo, info in libros.items():
+            print("isbn:", codigo, "titulo:", info["titulo"], "autor:", info["autor"], "genero:", info["genero"])
+
+# prueba
+agregar_libro("Cien años de soledad", "Garcia Marquez", "72345", "novela")
+agregar_libro("El principito", "Antoine", "72534", "infantil")
+
+print("TODOS LOS LIBROS:")
+mostrar_libros()
+
+print("\nBUSQUEDA:")
+print(buscar_libro("Marquez"))
